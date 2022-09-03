55c6510ff0e88fe13f926b9cd660151444fdbf14
Jonas Serafim <104076718+serajonas@users.noreply.github.com>
2022-09-03 11:56:48 -0300

    Views

--- CondominioTrevisoProj/condominio/urls.py
+++ CondominioTrevisoProj/condominio/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('condominio.urls')),
+    path('', views.index, name='index')
 ]
--- CondominioTrevisoProj/condominio/views.py
+++ CondominioTrevisoProj/condominio/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>Seja bem vindo</h1>")
 
+
+
 # Create your views here.
